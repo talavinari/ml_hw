6828b0de4e051329af231ecef2854cb18accf29b
Tal <talavinari@gmail.com>
2018-11-17 19:35:21 +0200

    hw3 part 1

--- hw3/skeleton_perceptron.py
+++ hw3/skeleton_perceptron.py
@@ -22,7 +22,7 @@ def perceptron(data, labels):
     """
 	returns: nd array of shape (data.shape[1],) or (data.shape[1],1) representing the perceptron classifier
     """
-
+    #TODO check if the zeros is same as nd array of shape 1
     w = np.zeros(shape=(len(data[0])), dtype=float)
 
     for index, xt in enumerate(data):
@@ -33,7 +33,6 @@ def perceptron(data, labels):
 
         true_label = labels[index]
         if true_label != predict:
-            # print("bad prediction - prediction is " + str(predict) + " real label is" + str(true_label))
             x = np.dot(xt, true_label)
             w = np.add(w, x)
 
@@ -64,29 +63,15 @@ def calc_accuracy(test_data, test_labels, w):
     return (test_size - mistake) / test_size
 
 
-if __name__ == '__main__':
-    np.set_printoptions(suppress=True)
+def exc_a():
     train_data, train_labels, validation_data, validation_labels, test_data, test_labels = sgd.helper()
-    print("train data len is " + str(len(train_data)))
-    print("validation data len is " + str(len(validation_data)))
-    print("test data len is " + str(len(test_data)))
-
-    print("vector size is " + str(len(train_data[0])))
-    print("labels are:" + str(validation_labels))
-    print(max(validation_labels))
-    print(min(validation_labels))
-
-    normalized_train_data = np.ndarray(shape=(len(train_data), len(train_data[0])), dtype=float)
-    for index, data in enumerate(train_data):
-        normalized_train_data[index] = normalize(data[:, np.newaxis], axis=0).ravel()
+    normalized_train_data = normalize_train_date(train_data)
 
     ns = [5, 10, 50, 100, 500, 1000, 5000]
-    results = np.ndarray(shape=(7, 2), dtype=float)
-
+    results = np.ndarray(shape=(7, 4), dtype=float)
     experiment_times = 100
-
     for index, n in enumerate(ns):
-        total_accuracy = 0
+        accuracies = []
         for i in range(experiment_times):
             array = normalized_train_data[:n]
             sliced_labels = train_labels[:n]
@@ -94,9 +79,59 @@ if __name__ == '__main__':
             array = array[p]
             sliced_labels = sliced_labels[p]
             w = perceptron(array, sliced_labels)
-            accuracy = calc_accuracy(test_data, test_labels, w)
-            total_accuracy += accuracy
+            accuracies.append(calc_accuracy(test_data, test_labels, w))
 
-        results[index] = [n, total_accuracy / experiment_times]
-
+        results[index] = [n, sum(accuracies) / experiment_times, np.percentile(accuracies, 5),
+                          np.percentile(accuracies, 95)]
     print(results)
+
+
+def normalize_train_date(train_data):
+    normalized_train_data = np.ndarray(shape=(len(train_data), len(train_data[0])), dtype=float)
+    for index, data in enumerate(train_data):
+        normalized_train_data[index] = normalize(data[:, np.newaxis], axis=0).ravel()
+    return normalized_train_data
+
+
+def find_wrong_images(test_data, test_labels, w):
+    wrong = []
+    for index, test in enumerate(test_data):
+        if np.dot(test, w) >= 0:
+            predict = 1
+        else:
+            predict = -1
+
+        if test_labels[index] != predict:
+            wrong.append(test)
+
+    return wrong
+
+
+def exc_b_c_d():
+    train_data, train_labels, validation_data, validation_labels, test_data, test_labels = sgd.helper()
+    normalized_train_data = normalize_train_date(train_data)
+    w = perceptron(normalized_train_data, train_labels)
+    accuracy = calc_accuracy(test_data, test_labels, w)
+    print("accuracy of the classifier trained on the full training set applied on the test set " + str(accuracy))
+
+    # plt.imshow(np.reshape(w, (28, 28)), interpolation='nearest')
+    # plt.show()
+    # wrong = find_wrong_images(test_data, test_labels, w)
+    # for wrong_pic in wrong:
+    #     plt.imshow(np.reshape(wrong_pic, (28, 28)), interpolation='nearest')
+    #     plt.show()
+
+
+if __name__ == '__main__':
+    np.set_printoptions(suppress=True)
+    exc_a()
+    exc_b_c_d()
+
+    # print("train data len is " + str(len(train_data)))
+    # print("validation data len is " + str(len(validation_data)))
+    # print("test data len is " + str(len(test_data)))
+    #
+    # print("vector size is " + str(len(train_data[0])))
+    # print("labels are:" + str(validation_labels))
+    # print(max(validation_labels))
+    # print(min(validation_labels))
