67120c41c1fb389110dc5bb9ec0cca3bb0338da3
Tal <talavinari@gmail.com>
2018-12-01 20:38:59 +0200

    hw4 part init

---
+++ hw4/skeleton_svm.py
@@ -0,0 +1,90 @@
+#################################
+# Your name:
+#################################
+
+# Please import and use stuff only from the packages numpy, sklearn, matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.datasets import make_blobs
+
+"""
+Q4.1 skeleton.
+
+Please use the provided functions signature for the SVM implementation.
+Feel free to add functions and other code, and submit this file with the name svm.py
+"""
+
+# generate points in 2D
+# return training_data, training_labels, validation_data, validation_labels
+def get_points():
+    X, y = make_blobs(n_samples=120, centers=2, random_state=0, cluster_std=0.88)
+    return X[:80], y[:80], X[80:], y[80:]
+
+
+def create_plot(X, y, clf):
+    plt.clf()
+
+    # plot the data points
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.PiYG)
+
+    # plot the decision function
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # create grid to evaluate model
+    xx = np.linspace(xlim[0] - 2, xlim[1] + 2, 30)
+    yy = np.linspace(ylim[0] - 2, ylim[1] + 2, 30)
+    YY, XX = np.meshgrid(yy, xx)
+    xy = np.vstack([XX.ravel(), YY.ravel()]).T
+    Z = clf.decision_function(xy).reshape(XX.shape)
+
+    # plot decision boundary and margins
+    ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+
+
+def train_three_kernels(X_train, y_train, X_val, y_val):
+    """
+    Returns: np.ndarray of shape (3,2) :
+                A two dimensional array of size 3 that contains the number of support vectors for each class(2) in the three kernels.
+    """
+
+    clf = svm.SVC(gamma='scale')
+    clf.fit(X_train, y_train)
+    svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+        decision_function_shape='ovr', degree=3, gamma='scale', kernel='rbf',
+        max_iter=-1, probability=False, random_state=None, shrinking=True,
+        tol=0.001, verbose=False)
+
+    predict = clf.predict(X_val)
+    print(predict)
+    print(y_val)
+
+
+def linear_accuracy_per_C(X_train, y_train, X_val, y_val):
+    """
+        Returns: np.ndarray of shape (11,) :
+                    An array that contains the accuracy of the resulting model on the VALIDATION set.
+    """
+    # TODO: add your code here
+
+
+def rbf_accuracy_per_gamma(X_train, y_train, X_val, y_val):
+    """
+        Returns: np.ndarray of shape (11,) :
+                    An array that contains the accuracy of the resulting model on the VALIDATION set.
+    """
+    # TODO: add your code here
+
+if __name__ == '__main__':
+    training_data, training_labels, validation_data, validation_labels = get_points()
+    # print(training_data)
+    # print(training_labels)
+    # print(validation_data)
+    # print(validation_labels)
+    train_three_kernels(training_data, training_labels, validation_data, validation_labels)
+
+
+
