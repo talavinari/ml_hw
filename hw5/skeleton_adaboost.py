2ae8040ee2cc5684940e5a9b768f2c5ecfcb75a7
Tal <talavinari@gmail.com>
2018-12-23 22:26:43 +0200

    hw5 init version

--- hw5/skeleton_adaboost.py
+++ hw5/skeleton_adaboost.py
@@ -1,15 +1,11 @@
 #################################
-# Your name:
+# Your name: Tal Avinari
 #################################
 
 import math
 
-# Please import and use stuff only from the packages numpy, sklearn, matplotlib.
-import matplotlib
-
-matplotlib.use('TkAgg')
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
 from process_data import parse_data
 
@@ -44,34 +40,23 @@ def ERMforDecisionStumps(current_distribution, X_train, y_train):
 
     for j in range(len(X_train[0])):
         column_values = X_train[:, j]
-        # argsort_by_x = np.argsort(column_values)
-        # sorted_y = y_train[argsort_by_x]
-        # distribution_by_sort = current_distribution[argsort_by_x]
-        # sorted_x = sorted(column_values)
-        # sorted_x.append(sorted_x[len(sorted_x) - 1] + 1)
-        #
         all_data = list(zip(column_values, y_train, current_distribution))
         sorted_data = sorted(all_data, key=lambda item: item[0])
 
-        # F = sum(distribution_by_sort[i] for i in range(len(X_train)) if sorted_y[i] == 1)
         F = sum(w for x, y, w in sorted_data if y == 1)
 
         if F < F_star:
             F_star = F
-            # tetha_star = sorted_x[0] - 1
             tetha_star = sorted_data[0][0] - 1
             j_star = j
         for i in range(len(X_train)):
-            # F = F - sorted_y[i] * distribution_by_sort[i]
             F = F - sorted_data[i][1] * sorted_data[i][2]
-            # if F < F_star and sorted_x[i] != sorted_x[i + 1]:
             if i + 1 == len(X_train):
                 next_x = sorted_data[i][0] + 1
             else:
                 next_x = sorted_data[i + 1][0]
             if F < F_star and sorted_data[i][0] != next_x:
                 F_star = F
-                # tetha_star = 0.5 * (sorted_x[i] + sorted_x[i + 1])
                 tetha_star = 0.5 * (sorted_data[i][0] + next_x)
                 j_star = j
 
@@ -87,13 +72,11 @@ def predict(h, x):
 def calc_error(Xs, labels, hypotheses, alpha_vals, t):
     errors = 0
     for x, y in zip(Xs, labels):
-        sign = 0
+        total = 0
         for i in range(t + 1):
-            sign += alpha_vals[i] * predict(hypotheses[i], x)
-        # for alpha, h in zip(alpha_vals, hypotheses):
-        #     sign += alpha * predict(h, x)
+            total += alpha_vals[i] * predict(hypotheses[i], x)
 
-        if sign * y < 0:
+        if total * y < 0:
             errors += 1
 
     return errors / len(Xs)
@@ -120,7 +103,6 @@ def run_adaboost(X_train, y_train, T):
 
     for t in range(T):
         index, tetha_star, e_t, predict_value = WL(current_distribution, X_train, y_train)
-        print("iteration " + str(t))
         h_t = Hypothesis(tetha_star, predict_value, index)
         alpha_t = 0.5 * np.log((1 - e_t) / e_t)
         print("hypothesis for T=" +
@@ -138,9 +120,20 @@ def run_adaboost(X_train, y_train, T):
         hypotheses.append((h_t.predict_value, h_t.index, h_t.threshold))
         alpha_vals.append(alpha_t)
 
-        print("train error : " + str(calc_error(X_train, y_train, hypotheses, alpha_vals, t)))
-
     return hypotheses, alpha_vals
+
+
+def calc_avg_exponent(Xs, labels, hypotheses, alpha_vals, t):
+    exponent_loss = 0
+
+    for x, y in zip(Xs, labels):
+        clf_total = 0
+        for i in range(t + 1):
+            clf_total += alpha_vals[i] * predict(hypotheses[i], x)
+
+        exponent_loss += np.exp(-1 * y * clf_total)
+
+    return exponent_loss / len(Xs)
 
 
 def main():
@@ -148,22 +141,42 @@ def main():
     if not data:
         return
     (X_train, y_train, X_test, y_test, vocab) = data
-    T = 5
-    experiment_results = np.ndarray(shape=(int(T), 3), dtype=float)
+    T = 80
+    experiment_results = np.ndarray(shape=(int(T), 5), dtype=float)
+
     hypotheses, alpha_vals = run_adaboost(X_train, y_train, T)
+
+    for h in hypotheses:
+        print("word: " + str(vocab.get(h[1])) + ", if lower than - " + str(h[2]) + " predict is: " + str(h[0]))
+
     for i in range(T):
         train_error = calc_error(X_train, y_train, hypotheses, alpha_vals, i)
         test_error = calc_error(X_test, y_test, hypotheses, alpha_vals, i)
-        experiment_results[i] = [i, train_error, test_error]
+        avg_exp_loss_train = calc_avg_exponent(X_train, y_train, hypotheses, alpha_vals, i)
+        avg_exp_loss_test = calc_avg_exponent(X_test, y_test, hypotheses, alpha_vals, i)
+        experiment_results[i] = [i, train_error, test_error, avg_exp_loss_train, avg_exp_loss_test]
 
     plt.title('Test error (red) & Train error (blue) as a function of T')
     plt.ylabel('error')
     plt.xlabel('T')
-    plt.plot(experiment_results[:, 0], experiment_results[:, 1], linestyle='dashdot', color='blue')
-    plt.plot(experiment_results[:, 0], experiment_results[:, 2], linestyle='dashdot', color='red')
+    plt.plot(experiment_results[:, 0], experiment_results[:, 1], color='blue')
+    plt.plot(experiment_results[:, 0], experiment_results[:, 2], color='red')
     plt.axis([0, T, 0, 0.4])
     plt.show()
 
+    plt.title('Average exponential loss - on test set (red) & on train set (blue) as a function of T')
+    plt.ylabel('exponential loss')
+    plt.xlabel('T')
+    plt.plot(experiment_results[:, 0], experiment_results[:, 3], color='blue')
+    plt.plot(experiment_results[:, 0], experiment_results[:, 4], color='red')
+    plt.axis([0, T, 0, 1])
+    plt.show()
+
+    print("train error:")
+    print(experiment_results[:, 1])
+
+    print("test error:")
+    print(experiment_results[:, 2])
 
 if __name__ == '__main__':
     main()
